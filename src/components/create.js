1c38478d7ffdf6f5b16658252360c2917821f89e
g00410404 <G00410404@atu.ie>
2023-10-20 14:52:58 +0000

    l4 done and commented

--- src/components/create.js
+++ src/components/create.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
-
 function Create() {
 
+    // Define state variables for title, cover, and author using the 'useState' hook
     const [title, setTitle] = useState('');
     const [cover, setCover] = useState('');
     const [author, setAuthor] = useState('');
 
+    // Define a function to handle the form submission
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Prevent the default form submission behavior
 
+        // Log the values of title, cover, and author when the form is submitted
         console.log("Title: " + title +
             " Cover: " + cover +
             " Author:" + author);
@@ -17,39 +19,42 @@ function Create() {
 
     return (
         <div>
-            <h2>Hello from create Component!</h2>
+            <h2>Hello from Create Component!</h2>
             <form onSubmit={handleSubmit}>
 
                 <div className="form-group">
                     <label>Add Book Title: </label>
-                    <input type="text"
+                    <input
+                        type="text"
                         className="form-control"
                         value={title}
-                        onChange={(e) => { setTitle(e.target.value) }}
+                        onChange={(e) => { setTitle(e.target.value) }} // Update the 'title' state when the input value changes
                     />
                 </div>
                 <div className="form-group">
                     <label>Add Book Cover: </label>
-                    <input type="text"
+                    <input
+                        type="text"
                         className="form-control"
                         value={cover}
-                        onChange={(e) => { setCover(e.target.value) }}
+                        onChange={(e) => { setCover(e.target.value) }} // Update the 'cover' state when the input value changes
                     />
                 </div>
                 <div className="form-group">
                     <label>Add Book Author: </label>
-                    <input type="text"
+                    <input
+                        type="text"
                         className="form-control"
                         value={author}
-                        onChange={(e) => { setAuthor(e.target.value) }}
+                        onChange={(e) => { setAuthor(e.target.value) }} // Update the 'author' state when the input value changes
                     />
                 </div>
                 <div>
-                    <input type="submit" value="Add Book"></input>
+                    <input type="submit" value="Add Book" /> {/* Submit button to trigger the 'handleSubmit' function */}
                 </div>
             </form>
         </div>
     );
-
 }
-export default Create;
+
+export default Create; // Export the 'Create' component
--- src/components/read.js
+++ src/components/read.js
@@ -1,36 +1,36 @@
 import { useEffect, useState } from "react";
-import Books from "./books";
+import Books from "./books"; 
 import axios from "axios";
 
 function Read() {
 
+    // Initialize a state variable 'data' using the 'useState' hook
     const [data, setData] = useState([]);
 
+    // Use the 'useEffect' hook to perform an HTTP GET request
     useEffect(
         () => {
             axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
                 .then(
                     (response) => {
-                        setData(response.data.books)
+                        setData(response.data.books); // Set the 'data' variable with the books data from the response
                     }
                 )
-
                 .catch(
                     (error) => {
-                        console.log(error);
+                        console.log(error); // Log any errors that occur during the HTTP request
                     }
                 );
-        }, []
+        }, [] // An empty dependency array means this effect will only run once when the component mounts
     );
-
-
 
     return (
         <div>
             <h2>Hello from Read Component!</h2>
-            <Books myBooks={data}></Books>
+            <Books myBooks={data}></Books> {/* Render the 'Books' component with the 'data' */}
         </div>
     );
 
 }
-export default Read;
+
+export default Read; // Export the 'Read' component
